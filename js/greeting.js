f33752773f0681f4fed11c99266becdf1825f9c7
jeeweb <52396673+jeeweb@users.noreply.github.com>
2020-09-30 23:43:23 +0900

    현재시간 추가

--- js/greeting.js
+++ js/greeting.js
@@ -1,17 +1,29 @@
 'use strict';
 
-const greeting = document.querySelectorAll('.section__greetings-msg > p');
+const greeting = document.querySelectorAll('.section__greetings-msg > p'),
+		clock = document.querySelector('.section__greetings-clock');
 let hours;
+let minutes;
+let sec;
+
 
 function checkTime() {
   const day = new Date();
-  hours = day.getHours();
+	hours = day.getHours();
+	minutes = day.getMinutes();
+	sec = day.getSeconds();
+	console.log(hours, minutes, sec)
 }
 //console.log(greeting);
 
-function loadMessage() {
-  checkTime();
+function showClock() {
+	checkTime();
+	clock.textContent = `${hours} : ${minutes} : ${sec}`;
 
+}
+
+function loadMessage() {
+	showClock();
   if (hours <= 12) {
     greeting[0].style.display = 'block';
   } else if (hours < 18) {
